4babc049c78b285ace8dccdde437ded4095f8235
Felipe Norato Lacerda <fnoratol@gmail.com>
2013-11-20 19:05:29 -0200

    Adicionada Classe Projeto

---
+++ src/main/Produto.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Produto {
+
+	private String nome;
+	private double valor_previsto;
+	private double valor_real;
+	private ArrayList<Double> precos_anteriores = new ArrayList<Double>();
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor_previsto() {
+		return valor_previsto;
+	}
+	public void setValor_previsto(double valor_previsto) {
+		this.valor_previsto = valor_previsto;
+	}
+	public double getValor_real() {
+		return valor_real;
+	}
+	public void setValor_real(double valor_real) {
+		this.valor_real = valor_real;
+	}
+	
+	public void atualiza() {
+		this.precos_anteriores.add(this.valor_previsto);
+		this.valor_previsto = this.valor_real;
+		
+	}
+	public ArrayList<Double> precos_anteriores() {
+		return this.precos_anteriores;
+		
+	}
+	
+	
+}
---
+++ src/test/TestProduto.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.ArrayList;
+
+import main.Produto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestProduto {
+
+
+	@Test
+	public void AdicionaProduto() {
+		Produto arroz = new Produto();
+		
+		arroz.setNome("Arroz");
+		arroz.setValor_previsto(6.99);
+		
+		Assert.assertEquals("Arroz",arroz.getNome());
+		Assert.assertEquals(6.99, arroz.getValor_previsto(), 0.001);
+	}
+	
+	@Test
+	public void AtualizaProdutonaCompra(){
+		Produto arroz = new Produto();
+		
+		arroz.setNome("Arroz");
+		arroz.setValor_previsto(6.99);
+		
+		arroz.setValor_real(7.40);
+		
+		ArrayList<Double> lista_teste = new ArrayList<Double>();
+		
+		Assert.assertEquals(lista_teste, arroz.precos_anteriores());
+		
+		lista_teste.add(arroz.getValor_previsto());
+		arroz.atualiza();
+		
+		Assert.assertEquals(7.40, arroz.getValor_previsto(), 0.001);
+		Assert.assertEquals(lista_teste, arroz.precos_anteriores());
+	}
+
+}
